74a4c05b18a32f8b97b2c3a3b1b71c960fee3bee
Roman Onyshkiv <romaonyshkiv@gmail.com>
2024-03-25 00:16:25 -0400

    bot added

--- cmd/bot.go
+++ cmd/bot.go
@@ -1,19 +1,27 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // botCmd represents the bot command
 var botCmd = &cobra.Command{
-	Use:   "bot",
-	Short: "A brief description of your command",
+	Use:     "bot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +29,34 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bot called")
+
+		fmt.Printf("bot %s is started", appVersion)
+
+		bot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Error %s", err)
+			return
+		}
+
+		bot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello, I'm bot %s", appVersion))
+
+			}
+
+			return err
+		})
+		bot.Start()
+
 	},
 }
 
