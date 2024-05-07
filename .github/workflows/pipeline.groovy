933952d5c4893fd23d03a76bb45c5646b3ab53ec
Roman Onyshkiv <romaonyshkiv@gmail.com>
2024-05-07 04:19:01 +0000

    groovy

---
+++ .github/workflows/pipeline.groovy
@@ -0,0 +1,31 @@
+pipeline {
+    agent any
+    parameters {
+
+        choice(name: 'OS', choices: ['linux', 'darwin', 'windows'], description: 'Pick OS')
+        choice(name: 'arch', choices: ['arm46', 'amd64'], description: 'Select arch')
+
+    }
+    stages {
+
+        stage('test') {
+            steps {
+                script {
+                    sh 'make test'
+                }
+            }
+        }
+
+
+        stage('create image') {
+            steps {
+                script {
+                    sh 'make image'
+                }
+
+            }
+        }
+
+        
+    }
+}
